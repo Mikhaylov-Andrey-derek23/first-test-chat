18cdb74e1ce3daa592f4c89ca537d6e814415e78
Mikhaylov-Andrey-derek23 <derek23@yandex.ru>
2021-04-08 14:07:08 +0300

    error cors

--- LongPulling.js
+++ LongPulling.js
@@ -8,16 +8,7 @@ const emmitter = new events.EventEmitter();
 
 const app = express();
 
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-})
+//app.use(cors());
 
 
 app.use(express.json());
